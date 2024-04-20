0810bb88b35f2db69accf3742ade6c41c87f9548
Arbios Morina <arbios.morina@student.uni-pr.edu>
2024-04-21 00:21:34 +0200

    Add encode

---
+++ Base32Test.java
@@ -0,0 +1,71 @@
+//package Base32Implementation;
+//package Base32Implementation;
+import java.io.*;
+import java.util.Scanner;
+
+
+//public class encrypt {
+//    public static void main(String[] args){
+//        System.out.println("Hello World");
+//    }
+//}
+
+@SuppressWarnings("unused")
+public class Base32Test {
+
+
+
+    public class base32Imp {
+        private static final String base32Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+        private static final int[] base32Lookup = { 0xFF, 0xFF, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                0xFF, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E,
+                0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+                0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                0xFF, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E,
+                0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+                0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+        };
+
+        /**
+         * Encodes byte array to Base32 String.
+         *
+         * @param bytes Bytes to encode.
+         * @return Encoded byte array <code>bytes</code> as a String.
+         *
+         */
+
+        static public void encode(final byte[] bytes) {
+            int i = 0, index = 0, digit = 0;
+            int currByte, nextByte;
+            StringBuffer base32 = new StringBuffer((bytes.length + 7) * 8 / 5);
+
+            while (i < bytes.length) {
+                currByte = (bytes[i] >= 0) ? bytes[i] : (bytes[i] + 256);
+
+                /* Is the current digit going to span a byte boundary? */
+                if (index > 3) {
+                    if ((i + 1) < bytes.length) {
+                        nextByte = (bytes[i + 1] >= 0)
+                                ? bytes[i + 1]
+                                : (bytes[i + 1] + 256);
+                    } else {
+                        nextByte = 0;
+                    }
+
+                    digit = currByte & (0xFF >> index);
+                    index = (index + 5) % 8;
+                    digit <<= index;
+                    digit |= nextByte >> (8 - index);
+                    i++;
+                }
+            }
+        }
+
+    }
+
+
+}
+
